e48eb79396f93eefcb5ef234015b5abb81b429cc
Andriy Havriluk <andriygavriluk@gmail.com>
2014-11-12 12:38:57 +0200

    Commit to update branch from master

--- webservice/app.js
+++ webservice/app.js
@@ -9,7 +9,7 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(logger('dev'));
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../ui')));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
--- webservice/handlers/article-temp.js
+++ webservice/handlers/article-temp.js
@@ -7,31 +7,33 @@ exports.addArticle = function(req, res) {
     var article = req.body;
     console.log('Adding article into temp: ' + JSON.stringify(article));
 
-    var newArticle = new ArticleTemp({
-        media: {
-            content: article.mediaContent
-        },
-        incident: {
-            time: article.incidentTime,
-            incidentType: article.incidentType,
-            coordinates: {
-                lat: article.incidentCoordinatesLat,
-                lon: article.incidentCoordinatesLon
+    if(!article.media.content || !article.incident.time || !article.incident.type || !article.incident.coordinates.lat || !article.incident.coordinates.lon || !article.incident.title) {
+        res.status(400).end();
+        throw "Bad request"
+    } else {
+        var newArticle = new ArticleTemp({
+            media: {
+                content: article.media.content
             },
-            title: article.incidentTitle
-        }
-    });
+            incident: {
+                time: article.incident.time,
+                incidentType: article.incident.type,
+                coordinates: {
+                    lat: article.incident.coordinates.lat,
+                    lon: article.incident.coordinates.lon
+                },
+                title: article.incident.title
+            }
+        });
 
-    newArticle.save(function(err, data) {
-        if(err) {
-            res.send({
-                message: 'Error',
-                err: err
-            })
-            throw err
-        }
-        res.send('Success');
-    });
+        newArticle.save(function(err, data) {
+            if(err) {
+                res.status(400).send(err);
+                throw err
+            }
+            res.send('Success');
+        });
+    }
 }
 
 exports.getAll = function(req, res) {
