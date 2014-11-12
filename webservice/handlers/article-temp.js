c63ce95d402cf381a7a80a47aeda5bb7b7e96a8c
Andriy Havriluk <andriygavriluk@gmail.com>
2014-11-12 12:50:41 +0200

    Changed supervisor on nodemon. Deleted result of merge conflict

--- webservice/handlers/article-temp.js
+++ webservice/handlers/article-temp.js
@@ -7,7 +7,6 @@ exports.addArticle = function(req, res) {
     var article = req.body;
     console.log('Adding article into temp: ' + JSON.stringify(article));
 
-<<<<<<< HEAD
     if(!article.media.content || !article.incident.time || !article.incident.type || !article.incident.coordinates.lat || !article.incident.coordinates.lon || !article.incident.title) {
         res.status(400).end();
         throw "Bad request"
@@ -26,22 +25,21 @@ exports.addArticle = function(req, res) {
                 title: article.incident.title
             }
         });
-=======
-    var newArticle = new ArticleTemp({
-        media: {
-            content: article.media.content
-        },
-        incident: {
-            time: article.incident.time,
-            incidentType: article.incident.type,
-            coordinates: {
-                lat: article.incident.coordinates.lat,
-                lon: article.incident.coordinates.lon
+
+        var newArticle = new ArticleTemp({
+            media: {
+                content: article.media.content
             },
-            title: article.incident.title
-        }
-    });
->>>>>>> 9c29bb954b157d599b3e2bd559cdbcef55ebaa35
+            incident: {
+                time: article.incident.time,
+                incidentType: article.incident.type,
+                coordinates: {
+                    lat: article.incident.coordinates.lat,
+                    lon: article.incident.coordinates.lon
+                },
+                title: article.incident.title
+            }
+        });
 
         newArticle.save(function(err, data) {
             if(err) {
