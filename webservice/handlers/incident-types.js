09f0d657a93c177dfd2e605fb9e1a64b43753508
Andriy Havriluk <andriygavriluk@gmail.com>
2014-11-26 11:38:59 +0200

    Added collection, view, template of incident types selects

--- webservice/handlers/incident-types.js
+++ webservice/handlers/incident-types.js
@@ -4,7 +4,6 @@ var fs = require('fs'),
 exports.getTypes = function(req, res) {
 	fs.readFile(path.normalize(__dirname + '/../db/data/incident-types.json'), {encoding: 'utf-8'}, function(err, type) {
 		if(err) throw err
-		console.log(type);
 		res.send(JSON.parse(type));
 	});
 }
