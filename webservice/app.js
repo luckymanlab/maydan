e93b1034a50731ec54a273f480e090b879d9c7f9
Andriy <andriygavriluk@gmail.com>
2014-11-28 15:18:41 +0200

    Update app.js
    Uncommented crossdomain staff

--- webservice/app.js
+++ webservice/app.js
@@ -11,11 +11,11 @@ app.use(bodyParser.json())
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, '../ui')));
 
-//app.use(function(req, res, next) {
-//    res.header("Access-Control-Allow-Origin", "*");
-//    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//    next();
-//});
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 app.use('/', router);
 
