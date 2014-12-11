901699a2d81fd2a102b229806ec0a360379d5a2e
Tabaka Vitaliy <vitaliy_tabaka@epam.com>
2014-12-11 18:04:01 +0200

    fixed indentation

--- cms/src/js/view/preview-article-view.js
+++ cms/src/js/view/preview-article-view.js
@@ -5,37 +5,41 @@ UT.ArticlePreviewView = Marionette.ItemView.extend({
     events: {
         'click .delete-item': 'destroyItem',
         'click .confirm-item': 'confirmItem',
-		'click #close-modal': 'destroy'
-	},   
-    initialize: function(model) {       
-        this.model = model;             
-        this.render();       
+        'click #close-modal': 'destroy'
+    },
+    initialize: function(model) {
+        this.model = model;
+        this.render();
     },
     onRender: function() {
-        var coordinates = this.model.attributes.incident.coordinates || {lat:0, lon:0}; 
+        var coordinates = this.model.attributes.incident.coordinates || {
+            lat: 0,
+            lon: 0
+        };
         this.showModal();
-        this.initMapCoordinate(coordinates.lat, coordinates.lon);        
+        this.initMapCoordinate(coordinates.lat, coordinates.lon);
     },
     onDestroy: function() {
-        this.$el.modal('hide'); 
+        this.$el.modal('hide');
     },
-    showModal: function () {
-    	this.$el.modal('show');
+    showModal: function() {
+        this.$el.modal('show');
         $('#article-modal').show();
     },
-    initMapCoordinate: function(lat, lon) {                         
-            var myLatlng = new google.maps.LatLng(lat, lon);
-            var mapOptions = {
-                zoom: 18,
-                draggable: false,
-                center: myLatlng,            
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map($('#map')[0], mapOptions);        
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map            
-            });        
+    initMapCoordinate: function(lat, lon) {
+        /* jslint nonew: false */
+        var myLatlng = new google.maps.LatLng(lat, lon);
+        var mapOptions = {
+            zoom: 18,
+            draggable: false,
+            center: myLatlng,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        new google.maps.Marker({
+            position: myLatlng,
+            map: new google.maps.Map($('#map')[0], mapOptions)
+        });
+        /* jslint nonew: true */
     },
     destroyItem: function(event) {
         this.destroy();
