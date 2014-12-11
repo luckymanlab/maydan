b7bd121b22a55e1b08620febf4e5e2c92d2fada3
Andriy Havriluk <andriygavriluk@gmail.com>
2014-12-11 14:42:34 +0200

    Added jquery cookie

--- ui/js/app.js
+++ ui/js/app.js
@@ -75,18 +75,7 @@ UT.ApplicationView = Backbone.View.extend({
 		this.articleModel.fetch();
 	},
 	getAccessToken: function() {
-		var key = 'accessToken=';
-		var ca = document.cookie.split(';');
-		for(var i = 0; i < ca.length; i ++) {
-			var c = ca[i];
-			while (c.charAt(0) === ' ') {
-				c = c.substring(1);
-			}
-			if (c.indexOf(key) !== -1) {
-				return c.substring(key.length, c.length);
-			}
-		}
-		return '';
+		return $.cookie('accessToken');
 	}
 });
 
