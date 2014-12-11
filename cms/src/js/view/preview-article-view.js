a0ddcea0c32b6455dbe8f5922b71f63379404f02
Tabaka Vitaliy <vitaliy_tabaka@epam.com>
2014-12-11 17:18:36 +0200

    add preview-article-view.js

---
+++ cms/src/js/view/preview-article-view.js
@@ -0,0 +1,48 @@
+var UT = window.UT || {};
+
+UT.ArticlePreviewView = Marionette.ItemView.extend({
+    template: "#article-preview-modal",
+    events: {
+        'click .delete-item': 'destroyItem',
+        'click .confirm-item': 'confirmItem',
+		'click #close-modal': 'destroy'
+	},   
+    initialize: function(model) {       
+        this.model = model;             
+        this.render();       
+    },
+    onRender: function() {
+        var coordinates = this.model.attributes.incident.coordinates || {lat:0, lon:0}; 
+        this.showModal();
+        this.initMapCoordinate(coordinates.lat, coordinates.lon);        
+    },
+    onDestroy: function() {
+        this.$el.modal('hide'); 
+    },
+    showModal: function () {
+    	this.$el.modal('show');
+        $('#article-modal').show();
+    },
+    initMapCoordinate: function(lat, lon) {                         
+            var myLatlng = new google.maps.LatLng(lat, lon);
+            var mapOptions = {
+                zoom: 18,
+                draggable: false,
+                center: myLatlng,            
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map($('#map')[0], mapOptions);        
+            var marker = new google.maps.Marker({
+                position: myLatlng,
+                map: map            
+            });        
+    },
+    destroyItem: function(event) {
+        this.destroy();
+        this.model.destroy();
+    },
+    confirmItem: function(event) {
+        this.destroy();
+        this.model.confirm();
+    }
+});
