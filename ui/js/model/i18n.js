487618183770bfd5993d7d3ec88efc65d3c50c50
Tabaka Vitaliy <vitaliy_tabaka@epam.com>
2014-12-18 16:22:45 +0200

    Add JSDoc coments

--- ui/js/model/i18n.js
+++ ui/js/model/i18n.js
@@ -1,22 +1,36 @@
-
+/*global UT, Backbone */
 var UT = window.UT || {};
 
+/**
+    Constructor of model.
+    @constructor
+    @extends Backbone.Model
+*/
 UT.I18n = Backbone.Model.extend({
-	baseURL: 'localization/',
-	initialize: function(lan) {
-		this.setLan(lan);
-	},
-	setLan: function(lan) {
-		var language = lan || 'en_US';
-		this.urlRoot = '' + this.baseURL + language + '.json';
-		this.fetch();
-	},
-	//function for finding {i18n.someText} end replace it to value of language.json
-	processTemplate: function(template) {
-		var that = this;
-		return template.replace(/{i18n\.(.+?)}/g, function(p1, p2) {
-			return that.get(p2);
-		});
-	}
+    baseURL: 'localization/',
+    initialize: function(lan) {
+        this.setLan(lan);
+    },
+    /**
+     * Set urlRoot form json file end baseURL
+     * @param {string} lan name of json file
+     */
+    setLan: function(lan) {
+        var language = lan || 'en_US';
+        this.urlRoot = '' + this.baseURL + language + '.json';
+        this.fetch();
+    },
+    /**
+     * Replace template data to localized properties of model
+     *
+     * @param {string} template that must be changed
+     * @deprecated
+     * @return {string} changed template
+     */
+    processTemplate: function(template) {
+        var that = this;
+        return template.replace(/{i18n\.(.+?)}/g, function(p1, p2) { // /{i18n\.(.+?)}/ === {i18n.someText}
+            return that.get(p2);
+        });
+    }
 });
-
