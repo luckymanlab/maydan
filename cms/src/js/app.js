81eeeef3c6bec5bc202d8dd4fb3bf465737ac9a9
Glib Belak <glib_belak@epam.com>
2015-01-29 16:35:29 +0200

    remove console.log in app.js

--- cms/src/js/app.js
+++ cms/src/js/app.js
@@ -4,7 +4,6 @@ UT.app = new Marionette.Application();
 
 UT.app.start = function(){
     UT.i18n = new UT.I18n();
-    console.log(UT.Config);
     UT.i18n.fetch({
         success: function(){
             this.articleTableView = new UT.ArticleTableView();
