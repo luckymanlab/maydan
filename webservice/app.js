9d6600bd6a3739ef14ad77e36718926785b27c6b
Andriy Havriluk <andriygavriluk@gmail.com>
2015-02-02 13:07:00 +0200

    Deleted all passport stuffs

--- webservice/app.js
+++ webservice/app.js
@@ -3,11 +3,8 @@ var express = require('express'),
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     cookieParser = require('cookie-parser'),
-    passport = require('passport'),
     cors = require('cors'),
-    session = require('express-session'),
-    router = require('./routes/index'),
-    auth = require('./routes/auth');
+    router = require('./routes/index');
 
 var app = express();
 
@@ -16,16 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(logger('dev'));
-app.use(session({
-    secret: 'a4f8071f-c873-4447-8ee2',
-    secure: false
-}));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(express.static(path.join(__dirname, './public')));
 
 app.use('/', router);
-app.use('/auth', auth);
 
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
